53383f457684fde1318d17facc3189beed74abe5
Muaz Aldairi <muaz-aldairi@live.nmit.ac.nz>
2020-03-10 20:39:19 +1300

    The second way

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ let z= 10;
 
 console.log(x**3 + y**3 +z**3);
 
+let firstCube = Math.pow(x,3);
+let secondCube= Math.pow(y,3);
+let thirdCube = Math.pow(z,3);
+
+console.log(firstCube + secondCube + thirdCube);
